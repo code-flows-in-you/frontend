34d1db426f3eae81879321f4f39d28cb662798ed
yuuoniy <linmq006@gmail.com>
2019-06-25 01:52:04 +0800

    fix avatar display

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -2,9 +2,9 @@
   <div>
     <el-row :gutter="20" style="height:50px;">
       <el-col :span="2" :offset="5">
-        <!-- <img src="../assets/logo.png" height="50px">
-         -->
-          <el-link href="./" :underline="false" ><img src="../assets/logo.png" height="50px"></el-link>
+        <el-link href="./" :underline="false">
+          <img src="../assets/logo.png" height="50px">
+        </el-link>
       </el-col>
       <el-col :span="4" :offset="5">
         <el-select
@@ -15,38 +15,37 @@
           placeholder="请输入关键词"
         ></el-select>
       </el-col>
-      <!-- <el-col :span="1">
-      <el-button type="text">问卷</el-button>
-    </el-col>
-    <el-col :span="1">
-      <el-button type="text">问答</el-button>
-    </el-col> -->
-    <el-col :span="4">
-      <el-menu :default-active="$route.path" router mode="horizontal" @select="handleSelect">
-        <el-menu-item index="/all">全部</el-menu-item>
-        <el-menu-item index="/quroa">问答</el-menu-item>
-        <el-menu-item index="/survey">问卷</el-menu-item>
-      </el-menu>
-    </el-col>
-    <el-col :span="1">
-      <!-- <img src="../assets/avatar.png" /> -->
-      <el-popover placement="top-start" width="40" trigger="hover">
-        <el-row>
-          <el-button type="text" icon="el-icon-user" @click="goToProfile">个人主页</el-button>
-        </el-row>
-        <el-row>
-          <el-button type="text" icon="el-icon-switch-button" @click="signout">退出</el-button>
-        </el-row>
-        <el-button style="margin-top:10px;" slot="reference" icon="el-icon-user-solid" @click="goToProfile" circle></el-button>
-      </el-popover>
-    </el-col>
-  </el-row>
-  <el-row :gutter="20" style="margin-bottom:50px; margin-top:20px">
-    <el-col :span="19" :offset="3">
-      <hr size=15 color="#99CCFF" style="box-shadow:0px 5px 6px 0px rgba(170, 170, 170, 1);opacity:0.67">
-    </el-col>
-  </el-row>
-</div>
+      <el-col :span="4">
+        <el-menu :default-active="$route.path" router mode="horizontal" @select="handleSelect">
+          <el-menu-item index="/all">全部</el-menu-item>
+          <el-menu-item index="/quroa">问答</el-menu-item>
+          <el-menu-item index="/survey">问卷</el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col :span="1">
+        <!-- <img src="../assets/avatar.png" /> -->
+        <el-popover placement="top-start" width="40" trigger="hover">
+          <el-row>
+            <el-button type="text" icon="el-icon-user" @click="goToProfile">个人主页</el-button>
+          </el-row>
+          <el-row>
+            <el-button type="text" icon="el-icon-switch-button" @click="signout">退出</el-button>
+          </el-row>
+          <el-button style="margin-top:10px;" slot="reference" icon="el-icon-user-solid" @click="goToProfile" circle></el-button>
+          <!-- <el-image :src="this.$store.state.user.Avatar" ></el-image> -->
+        </el-popover>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20" style="margin-bottom:50px; margin-top:20px">
+      <el-col :span="19" :offset="3">
+        <hr
+          size="15"
+          color="#99CCFF"
+          style="box-shadow:0px 5px 6px 0px rgba(170, 170, 170, 1);opacity:0.67"
+        >
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
